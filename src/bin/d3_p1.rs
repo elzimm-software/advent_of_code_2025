b65e4321b0f97b2c85bc26ba3b8f8c7f7bc443c1
Elijah Zimmerman <elijahz0409@gmail.com>
2025-12-08 10:03:30 -0500

    Complete day 3 part 1

---
+++ src/bin/d3_p1.rs
@@ -0,0 +1,49 @@
+use std::error::Error;
+use std::fs::read_to_string;
+
+const INPUT: &'static str = "987654321111111
+811111111111119
+234234234234278
+818181911112111
+";
+
+fn max_joltage(banks: &str) -> u32 {
+    let mut joltage = 0;
+    for line in banks.lines() {
+        let (max_first_index, max_first) = line
+            .get(0..line.len()-1)
+            .unwrap()
+            .chars()
+            .enumerate()
+            .fold((0, 0), |acc, (i, ch)| {
+                let n = ch.to_digit(10).unwrap();
+                if n > acc.1 {
+                    (i, n)
+                } else {
+                    acc
+                }
+            });
+        let max_second = line
+            .get(max_first_index+1..)
+            .unwrap()
+            .chars()
+            .fold(0, |acc, ch| {
+                let n = ch.to_digit(10).unwrap();
+                if n > acc {
+                    n
+                } else {
+                    acc
+                }
+            });
+        joltage += max_first * 10 + max_second;
+    }
+    joltage
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_to_string("inputs/d3_p1.txt")?;
+
+    println!("{}", max_joltage(&input));
+
+    Ok(())
+}
